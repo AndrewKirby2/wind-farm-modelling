6b11e888a45321026559691fc48cde739a4e70ea
AndrewKirby2 <72950277+AndrewKirby2@users.noreply.github.com>
2022-05-11 16:49:57 +0100

    Correct errors

--- predict_turbine_power_coefficient.py
+++ predict_turbine_power_coefficient.py
@@ -50,7 +50,7 @@ for i in range(6):
 
         U_Fprime = (-b + np.sqrt(b**2 - 4*a*c))/(2*a)
     
-        cp_les[run_no,i] = cp[run_no]*(U_Fprime/U_F)**3
+        cp_finite[run_no,i] = cp[run_no]*(U_Fprime/U_F)**3
 
         #calculate effective area ratio
         C_f0 = 0.28641758**2/(0.5*10.10348311**2)
@@ -76,44 +76,44 @@ for i in range(6):
 	    #use uncorrected ct_star to predict beta
 	    #analytical model gives ct_star = 0.75
 	    #divide by correction factor (N^2=0.8037111)
-	    ct_star_adj = 0.75 / 0.8037111
-            return ct_star_adj*effective_area_ratio_theory[i]*beta**2 + beta**gamma - 1 -zeta[i]*(1-beta)
+            ct_star_adj = 0.75 / 0.8037111
+            return ct_star_adj*effective_area_ratio_theory[run_no]*beta**2 + beta**gamma - 1 -zeta[i]*(1-beta)
 
-        beta = sp.bisect(NDFM,0,1)
-        cp_nishino[run_no,i] = 0.75**1.5 * beta**3 * 1.33**-0.5
+        beta_theory = sp.bisect(NDFM,0,1)
+        cp_nishino[run_no,i] = 0.75**1.5 * beta_theory**3 * 1.33**-0.5
 
 fig, ax = plt.subplots(nrows=3, ncols=2, figsize=[5.33,6.6], dpi=600)
-ax[0,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,0])
-pcm = ax[0,0].scatter(effective_area_ratio, training_data[:,8], s=5, c=training_data[:,3])
+ax[0,0].plot(effective_area_ratio_theory[12:], cp_nishino[12:,0])
+pcm = ax[0,0].scatter(effective_area_ratio, training_data[:,7], s=5, c=training_data[:,3])
 ax[0,0].set_xlabel(r'$\lambda/C_{f0}$')
 ax[0,0].set_ylabel(r'$C_p$')
 ax[0,0].set_title('a)', loc='left')
 
-ax[0,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,1])
+ax[0,1].plot(effective_area_ratio_theory[12:], cp_nishino[12:,1])
 ax[0,1].scatter(effective_area_ratio, cp_finite[:,1], s=5, c=training_data[:,3])
 ax[0,1].set_xlabel(r'$\lambda/C_{f0}$')
 ax[0,1].set_ylabel(r'$C_{p,finite}$')
 ax[0,1].set_title('b)', loc='left')
 
-ax[1,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,2])
+ax[1,0].plot(effective_area_ratio_theory[12:], cp_nishino[12:,2])
 ax[1,0].scatter(effective_area_ratio, cp_finite[:,2], s=5, c=training_data[:,3])
 ax[1,0].set_xlabel(r'$\lambda/C_{f0}$')
 ax[1,0].set_ylabel(r'$C_{p.finite}$')
 ax[1,0].set_title('c)', loc='left')
 
-ax[1,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,3])
+ax[1,1].plot(effective_area_ratio_theory[12:], cp_nishino[12:,3])
 ax[1,1].scatter(effective_area_ratio, cp_finite[:,3], s=5, c=training_data[:,3])
 ax[1,1].set_xlabel(r'$\lambda/C_{f0}$')
 ax[1,1].set_ylabel(r'$C_{p,finite}$')
 ax[1,1].set_title('d)', loc='left')
 
-ax[2,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,4])
+ax[2,0].plot(effective_area_ratio_theory[12:], cp_nishino[12:,4])
 ax[2,0].scatter(effective_area_ratio, cp_finite[:,4], s=5, c=training_data[:,3])
 ax[2,0].set_xlabel(r'$\lambda/C_{f0}$')
 ax[2,0].set_ylabel(r'$C_{p,finite}$')
 ax[2,0].set_title('e)', loc='left')
 
-ax[2,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,5])
+ax[2,1].plot(effective_area_ratio_theory[12:], cp_nishino[12:,5])
 ax[2,1].scatter(effective_area_ratio, cp_finite[:,5], s=5, c=training_data[:,3])
 ax[2,1].set_xlabel(r'$\lambda/C_{f0}$')
 ax[2,1].set_ylabel(r'$C_{p,finite}$')
@@ -125,4 +125,3 @@ cbar = fig.colorbar(pcm, ax=ax.ravel().tolist(), shrink=0.97)
 cbar.set_label(r'$C_T^*$')
 
 plt.savefig('LES_cp_results.png', bbox_inches='tight')
-

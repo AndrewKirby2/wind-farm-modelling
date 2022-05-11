3fcc557bcd8d8464d33eb3e5437775f2a27cc279
AndrewKirby2 <72950277+AndrewKirby2@users.noreply.github.com>
2022-05-11 16:16:48 +0100

    Plot results

--- predict_turbine_power_coefficient.py
+++ predict_turbine_power_coefficient.py
@@ -82,3 +82,47 @@ for i in range(6):
         beta = sp.bisect(NDFM,0,1)
         cp_nishino[run_no,i] = 0.75**1.5 * beta**3 * 1.33**-0.5
 
+fig, ax = plt.subplots(nrows=3, ncols=2, figsize=[5.33,6.6], dpi=600)
+ax[0,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,0])
+pcm = ax[0,0].scatter(effective_area_ratio, training_data[:,8], s=5, c=training_data[:,3])
+ax[0,0].set_xlabel(r'$\lambda/C_{f0}$')
+ax[0,0].set_ylabel(r'$C_p$')
+ax[0,0].set_title('a)', loc='left')
+
+ax[0,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,1])
+ax[0,1].scatter(effective_area_ratio, cp_finite[:,1], s=5, c=training_data[:,3])
+ax[0,1].set_xlabel(r'$\lambda/C_{f0}$')
+ax[0,1].set_ylabel(r'$C_{p,finite}$')
+ax[0,1].set_title('b)', loc='left')
+
+ax[1,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,2])
+ax[1,0].scatter(effective_area_ratio, cp_finite[:,2], s=5, c=training_data[:,3])
+ax[1,0].set_xlabel(r'$\lambda/C_{f0}$')
+ax[1,0].set_ylabel(r'$C_{p.finite}$')
+ax[1,0].set_title('c)', loc='left')
+
+ax[1,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,3])
+ax[1,1].scatter(effective_area_ratio, cp_finite[:,3], s=5, c=training_data[:,3])
+ax[1,1].set_xlabel(r'$\lambda/C_{f0}$')
+ax[1,1].set_ylabel(r'$C_{p,finite}$')
+ax[1,1].set_title('d)', loc='left')
+
+ax[2,0].plot(effective_area_ratio_theory[12:], cp_theory[12:,4])
+ax[2,0].scatter(effective_area_ratio, cp_finite[:,4], s=5, c=training_data[:,3])
+ax[2,0].set_xlabel(r'$\lambda/C_{f0}$')
+ax[2,0].set_ylabel(r'$C_{p,finite}$')
+ax[2,0].set_title('e)', loc='left')
+
+ax[2,1].plot(effective_area_ratio_theory[12:], cp_theory[12:,5])
+ax[2,1].scatter(effective_area_ratio, cp_finite[:,5], s=5, c=training_data[:,3])
+ax[2,1].set_xlabel(r'$\lambda/C_{f0}$')
+ax[2,1].set_ylabel(r'$C_{p,finite}$')
+ax[2,1].set_title('f)', loc='left')
+
+plt.tight_layout()
+
+cbar = fig.colorbar(pcm, ax=ax.ravel().tolist(), shrink=0.97)
+cbar.set_label(r'$C_T^*$')
+
+plt.savefig('LES_cp_results.png', bbox_inches='tight')
+
